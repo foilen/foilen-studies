ae789b3d83205a78774d8296effdfa7a7bcc9f45
Simon Levesque <simon@foilen.com>
2022-10-01 09:04:31 -0400

    Spring boot base

---
+++ src/main/java/com/foilen/etudes/EtudesApplication.java
@@ -0,0 +1,13 @@
+package com.foilen.etudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtudesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foilen/etudes/EtudesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foilen.etudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
