c57936426fcf6e2da5291514e9065a6cb55b8ff3
Simon Levesque <simon@foilen.com>
2022-11-19 10:42:31 -0500

    Vocabulary - Extract section that asks the words

---
+++ src/main/ui/src/common/AskWordsSection.js
@@ -0,0 +1,169 @@
+import playImage from "./play.png";
+import React, {useEffect, useState} from "react";
+
+/**
+ * Ask each word one after the other.
+ *
+ * props.wordsToFind: The words to ask one at a time
+ *
+ * props.onAnswer: (optional) The action to do for each answer (currentWord, currentAnswer, isSuccess)
+ * props.onCompletion: The action to do when everything was answered (finalScore)
+ *
+ */
+function AskWordsSection(props) {
+
+    // Main game
+    const [currentWordIdx, setCurrentWordIdx] = useState(0)
+
+    const [userAnswer, setUserAnswer] = useState({
+        answer: ''
+    })
+
+    // Progression
+    const [transition, setTransition] = useState(false)
+    const [correction, setCorrection] = useState(null)
+    const [score, setScore] = useState({
+        success: 0,
+        fails: 0,
+        total: props.wordsToFind.length,
+    })
+    const [wrongAnswers, setWrongAnswers] = useState([])
+
+    // Start
+    useEffect(() => {
+        console.log('Start from the top')
+        changeCurrentWord(0)
+    }, [props.wordsToFind])
+
+    function playMp3(speakTextCacheId) {
+        new Audio(`/speakText/${speakTextCacheId}`).play()
+    }
+
+    function updateAnswer(e) {
+        let value = e.target.value.toLowerCase()
+        setUserAnswer(prevState => ({...prevState, answer: value}))
+    }
+
+    function changeCurrentWord(idx) {
+        setCurrentWordIdx(idx)
+        setUserAnswer({answer: ''})
+        setTransition(false)
+        setCorrection(null)
+        let currentWord = props.wordsToFind[idx]
+        playMp3(currentWord.speakText.cacheId)
+    }
+
+    function validateAnswer() {
+        let currentWord = props.wordsToFind[currentWordIdx]
+        let expected = currentWord.word
+        let actual = userAnswer.answer.trim()
+
+        setTransition(true)
+        let nextScore = {...score};
+        let nextWrongAnswers = [...wrongAnswers]
+
+        let isSuccess = expected === actual;
+        if (isSuccess) {
+            ++nextScore.success
+            setCorrection({
+                isSuccess: true,
+                message: 'Bravo!',
+            })
+        } else {
+            ++nextScore.fails
+            setCorrection({
+                isSuccess: false,
+                message: expected,
+            })
+            nextWrongAnswers = [...nextWrongAnswers, currentWord]
+            setWrongAnswers(nextWrongAnswers)
+        }
+        setScore(nextScore)
+
+        // Send event
+        if (props.onAnswer) {
+            props.onAnswer(currentWord, actual, isSuccess)
+        }
+
+        // Timer and go to next or show summary
+        setTimeout(() => {
+            let nextWordIdx = currentWordIdx + 1
+            if (nextWordIdx === props.wordsToFind.length) {
+                // Show summary
+                const finalScore = {
+                    ...nextScore,
+                    wrongAnswers: nextWrongAnswers,
+                }
+                props.onCompletion(finalScore)
+            } else {
+                changeCurrentWord(nextWordIdx)
+            }
+        }, 3000)
+
+    }
+
+    return (
+        <div>
+            {props.wordsToFind.map((wordToFind, wIdx) =>
+                <div key={wordToFind.id}>
+                    {wIdx === currentWordIdx &&
+                        <div>
+                            <div className="row">
+                                <div className="col-12 col-sm-5 col-md-3">
+                                    <img src={playImage} alt="Dire le mot"
+                                         width="150px" height="150px"
+                                         className="mx-auto d-block"
+                                         onClick={() => playMp3(wordToFind.speakText.cacheId)}/>
+                                </div>
+                            </div>
+                            <hr/>
+                            <div className="row">
+                                <div className="col-10">
+                                    <input type="text" className="form-control"
+                                           autoComplete="off"
+                                           name="answer"
+                                           value={userAnswer.answer}
+                                           onChange={(e) => updateAnswer(e, userAnswer, setUserAnswer)}
+                                    />
+                                </div>
+                                <div className="col-1">
+                                    <button className="btn btn-primary"
+                                            disabled={transition}
+                                            onClick={() => validateAnswer()}>OK
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    }
+                </div>
+            )}
+
+            { /* Show if the current answer is good */}
+            {correction && <>
+                <hr/>
+
+                <div className="row">
+                    <div className="col-12">
+                        <p className={`${correction.isSuccess ? 'text-bg-success' : 'text-bg-danger'}`}>{correction.message}</p>
+                    </div>
+                </div>
+            </>
+            }
+
+            <hr/>
+
+            { /* Show the progression */}
+            <div className="row">
+                <div className="col-12">
+                    Progression:
+                    <span className="text-bg-success">{score.success}</span> ;
+                    <span className="text-bg-danger">{score.fails}</span> ;
+                    {score.success + score.fails} / {score.total}
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AskWordsSection
--- src/main/ui/src/vocabulary/VocabularyPage.js
+++ src/main/ui/src/vocabulary/VocabularyPage.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from "react";
 import {autoRetry} from "../service";
 import lodash from "lodash";
-import playImage from "./play.png";
 import {NavLink} from "react-router-dom";
 import "./VocabularyPage.css"
 import {toast} from "react-toastify";
+import AskWordsSection from "../common/AskWordsSection";
 
 function VocabularyPage() {
 
@@ -13,18 +13,6 @@ function VocabularyPage() {
 
     // Main game
     const [wordsToFind, setWordsToFind] = useState(null)
-    const [currentWordIdx, setCurrentWordIdx] = useState(null)
-
-    const [userAnswer, setUserAnswer] = useState({
-        answer: ''
-    })
-
-    // Progression
-    const [score, setScore] = useState(null)
-
-    const [transition, setTransition] = useState(false)
-    const [correction, setCorrection] = useState(null)
-    const [wrongAnswers, setWrongAnswers] = useState(null)
 
     // End of game
     const [finalScore, setFinalScore] = useState(null)
@@ -103,88 +91,24 @@ function VocabularyPage() {
     function startGame(words) {
         words = lodash.shuffle(words)
         setWordsToFind(words)
-        changeCurrentWord(0, words)
-        setScore({
-            success: 0,
-            fails: 0,
-            total: words.length,
-        })
-        setWrongAnswers([])
         setFinalScore(null)
     }
 
-    function changeCurrentWord(idx, items) {
-        if (items === undefined) {
-            items = wordsToFind;
-        }
-        setCurrentWordIdx(idx)
-        setUserAnswer({answer: ''})
-        setTransition(false)
-        setCorrection(null)
-        let currentWord = items[idx]
-        playMp3(currentWord.speakText.cacheId)
-    }
+    function onAnswer(currentWord, currentAnswer, isSuccess) {
 
-    function playMp3(speakTextCacheId) {
-        new Audio(`/speakText/${speakTextCacheId}`).play()
-    }
-
-    function validateAnswer() {
-        let currentWord = wordsToFind[currentWordIdx]
-        let expected = currentWord.word
-        let actual = userAnswer.answer.trim()
-
-        setTransition(true)
-        let nextScore = {...score};
-        let nextWrongAnswers = [...wrongAnswers]
         const trackForm = {
             wordId: currentWord.id,
-            answer: actual,
+            answer: currentAnswer,
+            success: isSuccess,
         }
-        if (expected === actual) {
-            // TODO Happy sound
-            ++nextScore.success
-            setCorrection({
-                isSuccess: true,
-                message: 'Bravo!',
-            })
-            trackForm.success = true
-        } else {
-            // TODO Sad sound
-            ++nextScore.fails
-            setCorrection({
-                isSuccess: false,
-                message: expected,
-            })
-            nextWrongAnswers = [...nextWrongAnswers, currentWord]
-            setWrongAnswers(nextWrongAnswers)
-            trackForm.success = false
-        }
-        setScore(nextScore)
 
         // Send tracking
         autoRetry('Send tracking', () => window.service.wordListTrack(trackForm), 5000)
-
-        // Timer and go to next or show summary
-        setTimeout(() => {
-            let nextWordIdx = currentWordIdx + 1
-            if (nextWordIdx === wordsToFind.length) {
-                // Show summary
-                setWordsToFind(null)
-                setFinalScore({
-                    ...nextScore,
-                    wrongAnswers: nextWrongAnswers,
-                })
-            } else {
-                changeCurrentWord(nextWordIdx)
-            }
-        }, 3000)
-
     }
 
-    function updateAnswer(e) {
-        let value = e.target.value.toLowerCase()
-        setUserAnswer(prevState => ({...prevState, answer: value}))
+    function onCompletion(gameFinalScore) {
+        setWordsToFind(null)
+        setFinalScore(gameFinalScore)
     }
 
     return (
@@ -297,68 +221,11 @@ function VocabularyPage() {
                 }
 
                 { /* The main game */}
-                {wordsToFind &&
-                    <div>
-                        {wordsToFind.map((wordToFind, wIdx) =>
-                            <div key={wordToFind.id}>
-                                {wIdx === currentWordIdx &&
-                                    <div>
-                                        <div className="row">
-                                            <div className="col-12 col-sm-5 col-md-3">
-                                                <img src={playImage} alt="Dire le mot"
-                                                     width="150px" height="150px"
-                                                     className="mx-auto d-block"
-                                                     onClick={() => playMp3(wordToFind.speakText.cacheId)}/>
-                                            </div>
-                                        </div>
-                                        <hr/>
-                                        <div className="row">
-                                            <div className="col-10">
-                                                <input type="text" className="form-control"
-                                                       autoComplete="off"
-                                                       name="answer"
-                                                       value={userAnswer.answer}
-                                                       onChange={(e) => updateAnswer(e, userAnswer, setUserAnswer)}
-                                                />
-                                            </div>
-                                            <div className="col-1">
-                                                <button className="btn btn-primary"
-                                                        disabled={transition}
-                                                        onClick={() => validateAnswer()}>OK
-                                                </button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                }
-                            </div>
-                        )}
-
-                        { /* Show if the current answer is good */}
-                        {correction && <>
-                            <hr/>
-
-                            <div className="row">
-                                <div className="col-12">
-                                    <p className={`${correction.isSuccess ? 'text-bg-success' : 'text-bg-danger'}`}>{correction.message}</p>
-                                </div>
-                            </div>
-                        </>
-                        }
-
-                        <hr/>
-
-                        { /* Show the progression */}
-                        <div className="row">
-                            <div className="col-12">
-                                Progression:
-                                <span className="text-bg-success">{score.success}</span> ;
-                                <span className="text-bg-danger">{score.fails}</span> ;
-                                {score.success + score.fails} / {score.total}
-                            </div>
-                        </div>
-
-                    </div>
-                }
+                {wordsToFind && <AskWordsSection
+                    wordsToFind={wordsToFind}
+                    onAnswer={onAnswer}
+                    onCompletion={onCompletion}
+                />}
 
                 { /* The summary view */}
                 {finalScore &&
