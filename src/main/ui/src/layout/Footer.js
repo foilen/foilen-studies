03e403c9ca14d75b90967190666de646280787d0
Simon Levesque <simon@foilen.com>
2024-02-03 10:06:06 -0500

    Library upgrades

--- src/main/ui/src/layout/Footer.js
+++ src/main/ui/src/layout/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div className="row">
             <div className="col">
                 <footer>
-                    <p>&copy; <a href="https://foilen.com" target="_blank" rel="noreferrer">Foilen</a> 2022-2022</p>
+                    <p>&copy; <a href="https://foilen.com" target="_blank" rel="noreferrer">Foilen</a> 2022-2024</p>
                 </footer>
             </div>
         </div>
