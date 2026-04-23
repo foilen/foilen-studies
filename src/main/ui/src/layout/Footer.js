e91b14be2da982e52924233c49b78d9b9590545b
Simon Levesque <simon@foilen.com>
2026-04-23 19:03:30 -0400

    Copyright year

--- src/main/ui/src/layout/Footer.js
+++ src/main/ui/src/layout/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
         <div className="row">
             <div className="col">
                 <footer>
-                    <p>&copy; <a href="https://foilen.com" target="_blank" rel="noreferrer">Foilen</a> 2022-2025</p>
+                    <p>&copy; <a href="https://foilen.com" target="_blank" rel="noreferrer">Foilen</a> 2022-2026</p>
                 </footer>
             </div>
         </div>
